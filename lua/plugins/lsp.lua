c5e9b1d8928f44d0d7b34908c152103ebcf0da68
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2024-07-11 16:54:58 -0300

    config lua lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,89 +1,116 @@
 return {
-		{
-		{
-			'VonHeikemen/lsp-zero.nvim',
-			branch = 'v3.x',
-			lazy = true,
-			config = false,
-			init = function()
-				-- Disable automatic setup, we are doing it manually
-				vim.g.lsp_zero_extend_cmp = 0
-				vim.g.lsp_zero_extend_lspconfig = 0
-			end,
-		},
-		{
-			'williamboman/mason.nvim',
-			lazy = false,
-			config = true,
-		},
+    {
+        {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v3.x',
+            lazy = true,
+            config = false,
+            init = function()
+                -- Disable automatic setup, we are doing it manually
+                vim.g.lsp_zero_extend_cmp = 0
+                vim.g.lsp_zero_extend_lspconfig = 0
+            end,
+        },
+        {
+            'williamboman/mason.nvim',
+            lazy = false,
+            config = true,
+        },
 
-		-- Autocompletion
-		{
-			'hrsh7th/nvim-cmp',
-			event = 'InsertEnter',
-			dependencies = {
-				{'L3MON4D3/LuaSnip'},
-			},
-			config = function()
-				-- Here is where you configure the autocompletion settings.
-				local lsp_zero = require('lsp-zero')
-				lsp_zero.extend_cmp()
+        -- Autocompletion
+        {
+            'hrsh7th/nvim-cmp',
+            event = 'InsertEnter',
+            dependencies = {
+                {'L3MON4D3/LuaSnip'},
+            },
+            config = function()
+                -- Here is where you configure the autocompletion settings.
+                local lsp_zero = require('lsp-zero')
+                lsp_zero.extend_cmp()
 
-				-- And you can configure cmp even more, if you want to.
-				local cmp = require('cmp')
-				local cmp_action = lsp_zero.cmp_action()
+                -- And you can configure cmp even more, if you want to.
+                local cmp = require('cmp')
+                local cmp_action = lsp_zero.cmp_action()
 
-				cmp.setup({
-					formatting = lsp_zero.cmp_format({details = true}),
-					mapping = cmp.mapping.preset.insert({
-						['<C-Space>'] = cmp.mapping.complete(),
-						['<C-u>'] = cmp.mapping.scroll_docs(-4),
-						['<C-d>'] = cmp.mapping.scroll_docs(4),
-						['<C-f>'] = cmp_action.luasnip_jump_forward(),
-						['<C-b>'] = cmp_action.luasnip_jump_backward(),
-					}),
-					snippet = {
-						expand = function(args)
-							require('luasnip').lsp_expand(args.body)
-						end,
-					},
-				})
-			end
-		},
+                cmp.setup({
+                    formatting = lsp_zero.cmp_format({details = true}),
+                    mapping = cmp.mapping.preset.insert({
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    }),
+                    snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body)
+                        end,
+                    },
+                })
+            end
+        },
 
-		-- LSP
-		{
-			'neovim/nvim-lspconfig',
-			cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-			event = {'BufReadPre', 'BufNewFile'},
-			dependencies = {
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'williamboman/mason-lspconfig.nvim'},
-			},
-			config = function()
-				-- This is where all the LSP shenanigans will live
-				local lsp_zero = require('lsp-zero')
-				lsp_zero.extend_lspconfig()
+        -- LSP
+        {
+            'neovim/nvim-lspconfig',
+            cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+            event = {'BufReadPre', 'BufNewFile'},
+            dependencies = {
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'williamboman/mason-lspconfig.nvim'},
+            },
+            config = function()
+                -- This is where all the LSP shenanigans will live
+                local lsp_zero = require('lsp-zero')
+                lsp_zero.extend_lspconfig()
 
-				--- if you want to know more about lsp-zero and mason.nvim
-				--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-				lsp_zero.on_attach(function(client, bufnr)
-					-- see :help lsp-zero-keybindings
-					-- to learn the available actions
-					lsp_zero.default_keymaps({buffer = bufnr})
-				end)
+                --- if you want to know more about lsp-zero and mason.nvim
+                --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+                lsp_zero.on_attach(function(client, bufnr)
+                    -- see :help lsp-zero-keybindings
+                    -- to learn the available actions
+                    lsp_zero.default_keymaps({buffer = bufnr})
+                end)
 
-				require('mason-lspconfig').setup({
-					ensure_installed = {},
-					handlers = {
-						-- this first function is the "default handler"
-						-- it applies to every language server without a "custom handler"
-						function(server_name)
-							require('lspconfig')[server_name].setup({})
-						end,
-					}
-				})
-			end
-		}
-	}
-}
+                require('mason-lspconfig').setup({
+                    ensure_installed = {},
+                    handlers = {
+                        lua_ls = function ()
+                            require('lspconfig').lua_ls.setup({
+                                settings = {
+                                    Lua = {}
+                                },
+                                on_init = function(client)
+                                    local uv = vim.uv or vim.loop
+                                    local path = client.workspace_folders[1].name
+
+                                    -- Don't do anything if there is a project local config
+                                    if uv.fs_stat(path .. '/.luarc.json') 
+                                        or uv.fs_stat(path .. '/.luarc.jsonc')
+                                        then
+                                            return
+                                        end
+
+                                        -- Apply neovim specific settings
+                                        local lua_opts = lsp_zero.nvim_lua_ls()
+
+                                        client.config.settings.Lua = vim.tbl_deep_extend(
+                                        'force',
+                                        client.config.settings.Lua,
+                                        lua_opts.settings.Lua
+                                        )
+                                    end,
+                                })
+                            end,
+                            -- this first function is the "default handler"
+                            -- it applies to every language server without a "custom handler"
+                            function(server_name)
+                                require('lspconfig')[server_name].setup({})
+                            end,
+                        }
+                    })
+                end
+            }
+        }
+    }
