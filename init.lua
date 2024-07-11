04a6663a14ff5b83f09dc3ef478c49d27488eabd
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2024-07-11 16:37:01 -0300

    add autocompletion

--- init.lua
+++ init.lua
@@ -33,46 +33,54 @@ require("lazy").setup({
 vim.opt.termguicolors = true
 vim.cmd.colorscheme("rose-pine")
 
-local lsp_zero = require("lsp-zero")
+
+---
+-- LSP setup
+---
+local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({buffer = bufnr})
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require("mason").setup({})
-require("mason-lspconfig").setup({
-	handlers = {
-		lua_ls = function()
-			require('lspconfig').lua_ls.setup({
-				settings = {
-					Lua = {}
-				},
-				on_init = function(client)
-					local uv = vim.uv or vim.loop
-					local path = client.workspace_folders[1].name
+--- if you want to know more about lsp-zero and mason.nvim
+--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
+})
 
-					-- Don't do anything if there is a project local config
-					if uv.fs_stat(path .. '/.luarc.json')
-						or uv.fs_stat(path .. '/.luarc.jsonc')
-						then
-							return
-						end
+---
+-- Autocompletion config
+---
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
 
-						-- Apply neovim specific settings
-						local lua_opts = lsp_zero.nvim_lua_ls()
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
 
-						client.config.settings.Lua = vim.tbl_deep_extend(
-						'force',
-						client.config.settings.Lua,
-						lua_opts.settings.Lua
-						)
-					end,
-				})
-			end,
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-		},
-	})
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
