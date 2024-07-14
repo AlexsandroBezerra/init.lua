e6619e610f255a358a876b5a9d31cf01f46269aa
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2024-07-13 21:24:27 -0300

    use catppuccin theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,12 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
 		config = function()
 			vim.opt.termguicolors = true
-			vim.cmd.colorscheme("rose-pine")
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	}
 }
