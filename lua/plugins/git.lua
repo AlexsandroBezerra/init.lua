047fe5a52964c4919c2b076d724477e49f57fd33
Alexsandro Bezerra <alexsandro.g.bezerra@gmail.com>
2024-09-19 10:07:24 -0300

    change git plugin to fugitive

---
+++ lua/plugins/git.lua
@@ -0,0 +1,9 @@
+return {
+    "tpope/vim-fugitive",
+    keys = {
+        { "<leader>gs", "<cmd>G<cr>", desc = "Git status" },
+        { "<leader>gc", "<cmd>G commit<cr>", desc = "Git commit" },
+        { "<leader>gp", "<cmd>G pull<cr>", desc = "Git pull" },
+        { "<leader>gP", "<cmd>G push<cr>", desc = "Git push" },
+    }
+}
